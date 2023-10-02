ec6f5e23944656a211b48fdadb44f9c7199f4fc9
Iury <iury211@gmail.com>
2023-10-02 14:46:43 -0300

    Adicionando a classe principal do Monolito

---
+++ Principal.java
@@ -0,0 +1,13 @@
+package monolito;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Usuario usuario = new Usuario(1,"Iury Ribeiro", "iury211@gmail.com" , "000.000.000-00", "av meia duzia 2785", "68900-000");
+		System.out.println(usuario);
+			
+		
+	}
+
+}
